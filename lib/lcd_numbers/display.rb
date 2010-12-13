146deb9ba1f392961e4ec746d39ed4985306d80f
jbeck <justinbeck@mac.com>
2010-12-12 22:27:04 -0500

    Refactored slightly.

--- lib/lcd_numbers/display.rb
+++ lib/lcd_numbers/display.rb
@@ -14,48 +14,51 @@ module LcdNumbers
     
     def print_num(segment_size, number)
       @size = segment_size
-      @numbers = digit_array(number)
+      @numbers = number.to_s.split(%r{\s*}).collect { |s| s.to_i }
       
-      calculate_digit_size
-      print
+      @row_count = (2 * @size) + 1
+      @digit_col_count = @size + 2
+      @median = ((@row_count - 1) / 2)
+      
+      generate_ascii_rows
     end
     
     private
     
-    def print
+    def generate_ascii_rows
       rows = String.new
       
       @row_count.times do |r|
-        rows += build_row(r)
+        rows += process_row(r)
         rows += "\n" unless r == (@row_count - 1)
       end
       
       @output.puts rows
     end
     
-    def build_row(r)
+    def process_row(r)
       @row = String.new
       
       @numbers.each do |number|
         @number = number
         
         if r == 0
-          process_row([nil, 1, nil])
+          construct_row([nil, 1, nil])
         elsif r < @median
-          process_row([2, nil, 4])
+          construct_row([2, nil, 4])
         elsif r == @median
-          process_row([2, 3, 4])
+          construct_row([2, 3, 4])
         elsif r > @median and r < (@row_count - 1)
-          process_row([5, nil, 7])
+          construct_row([5, nil, 7])
         else
-          process_row([5, 6, 7])
+          construct_row([5, 6, 7])
         end
       end
       
       @row
     end
     
-    def process_row(segments)
+    def construct_row(segments)
       @digit_col_count.times do |c|
         if c == 0
           @row += SEGMENTS_MAP[@number].include?(segments[0]) ? "|" : " "
@@ -67,17 +70,6 @@ module LcdNumbers
       end
     end
     
-    def calculate_digit_size
-      @row_count = (2 * @size) + 1
-      @digit_col_count = @size + 2
-      @total_col_count = @number.to_s.length == 1 ? @digit_col_count : ((@number.to_s.length + 1) * @digit_col_count) - 1
-      @median = ((@row_count - 1) / 2)
-    end
-    
-    def digit_array(number)
-      number.to_s.split(%r{\s*}).collect { |s| s.to_i }
-    end
-
     SEGMENTS_MAP = {
       1 => [ 4, 7 ],
       2 => [ 1, 3, 4, 5, 6 ],
