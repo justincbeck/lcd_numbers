9c30e93794187a9695e34b1dae928ea2ec5b2035
jbeck <justinbeck@mac.com>
2010-12-12 20:22:05 -0500

    Refactored.

--- lib/lcd_numbers/display.rb
+++ lib/lcd_numbers/display.rb
@@ -1,23 +1,28 @@
+# This module prints a single digit as an LCD segment display
+# The segments are numbered as follows:
+#   1
+# 2   4
+#   3
+# 5   7
+#   6
+
 module LcdNumbers
   class Display
     def initialize(output)
       @output = output
     end
     
-    def print_num(size, number)
-      @row_count = (2 * size) + 1
-      @digit_col_count = size + 2
-      @total_col_count = ((number.to_s.length + 1) * @digit_col_count) - 1
-      @median = ((@row_count - 1) / 2)
-
-      @segments = SEGMENTS_MAP[number]
-
-      print(size)
+    def print_num(segment_size, number)
+      @size = segment_size
+      @number = number
+      
+      calculate_digit_size
+      print
     end
     
     private
-
-    def print(size)
+    
+    def print
       rows = String.new
       
       @row_count.times do |r|
@@ -32,86 +37,51 @@ module LcdNumbers
       @row = String.new
       
       if r == 0
-        # We're dealing with segment 1
-        @digit_col_count.times do |c|
-          if c == 0
-            # It's the first col of @row 1 which is always blank
-            @row += " "
-          elsif @segments.include?(1) and c != (@digit_col_count - 1)
-            # We have a segment 1 and it's not the last col
-            @row += "_"
-          else
-            # It's the last col of @row 1 which is always blank
-            @row += " "
-          end
-        end
+        process_row([nil, 1, nil])
       elsif r < @median
-        # We're not to segment 3 yet but we're building segments 2 and 4
-        do_some_shit([2, nil, 4])
+        process_row([2, nil, 4])
       elsif r == @median
-        # We're building segment 3 (and the bottom of segments 2 and 4)
-        do_some_other_shit([2, 3, 4])
+        process_row([2, 3, 4])
       elsif r > @median and r < (@row_count - 1)
-        # We're not to segment 6 yet but we're building segments 5 and 7
-        do_some_shit([5, nil, 7])
+        process_row([5, nil, 7])
       else
-        # We're building segment 6 (and the bottom of segments 5 and 7)
-        do_some_other_shit([5, 6, 7])
+        process_row([5, 6, 7])
       end
       
       @row
     end
     
-    def do_some_shit(segments)
-      @digit_col_count.times do |c|
-        if @segments.include?(segments[0]) and c == 0
-          # We have a vertical segment and it's the first col of the @row
-          @row += "|"
-        elsif c < (@digit_col_count - 1)
-          # We don't have a vertical segment or a horizontal segment
-          @row += " "
-        else
-          # It's the last col of the @row
-          if @segments.include?(segments[2])
-            # We have a vertical segment
-            @row += "|"
-          else
-            # We don't have a vertical
-            @row += " "
-          end
-        end
-      end
-    end
-    
-    def do_some_other_shit(segments)
+    def process_row(segments)
       @digit_col_count.times do |c|
         if c == 0
-          if @segments.include?(segments[0])
-            # We have a segment 5 and it's the first col of the @row
+          if SEGMENTS_MAP[@number].include?(segments[0])
             @row += "|"
-          elsif c < (@digit_col_count - 1)
-            # We're between segments 5 and 7 which is always blank
+          else
             @row += " "
           end
-        elsif c != (@digit_col_count - 1)
-          if @segments.include?(segments[1])
+        elsif c < (@digit_col_count - 1)
+          if SEGMENTS_MAP[@number].include?(segments[1])
             @row += "_"
           else
             @row += " "
           end
         else
-          # It's the last col of the @row
-          if @segments.include?(segments[2])
-            # We have a vertical segment
+          if SEGMENTS_MAP[@number].include?(segments[2])
             @row += "|"
           else
-            # We don't have a vertical
             @row += " "
           end
         end
       end
     end
     
+    def calculate_digit_size
+      @row_count = (2 * @size) + 1
+      @digit_col_count = @size + 2
+      @total_col_count = ((@number.to_s.length + 1) * @digit_col_count) - 1
+      @median = ((@row_count - 1) / 2)
+    end
+
     SEGMENTS_MAP = {
       1 => [ 4, 7 ],
       2 => [ 1, 3, 4, 5, 6 ],
